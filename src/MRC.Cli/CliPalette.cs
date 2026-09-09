c8ea202b6c94c5f4fb065d48c202e08e7461aef8
doonchy16-cloud <doonchy16@gmail.com>
2026-09-09 12:33:19 -0800

    feat: define semantic CLI palette

---
+++ src/MRC.Cli/CliPalette.cs
@@ -0,0 +1,29 @@
+namespace MRC.Cli;
+
+public enum CliTone
+{
+    Normal = 0,
+    Heading = 1,
+    Success = 2,
+    Warning = 3,
+    Error = 4,
+    Path = 5,
+    Metadata = 6,
+    Secondary = 7
+}
+
+public static class CliPalette
+{
+    public static ConsoleColor ColorFor(CliTone tone) => tone switch
+    {
+        CliTone.Normal => ConsoleColor.White,
+        CliTone.Heading => ConsoleColor.Cyan,
+        CliTone.Success => ConsoleColor.Green,
+        CliTone.Warning => ConsoleColor.Yellow,
+        CliTone.Error => ConsoleColor.Red,
+        CliTone.Path => ConsoleColor.Cyan,
+        CliTone.Metadata => ConsoleColor.Magenta,
+        CliTone.Secondary => ConsoleColor.DarkGray,
+        _ => ConsoleColor.White
+    };
+}
