115d2a8b9bde724276373103d53f287967a4b2b9
doonchy16-cloud <doonchy16@gmail.com>
2026-09-09 03:12:40 -0800

    fix: use actual Main-PC Windows identity

--- src/MRC.Core/MrcConstants.cs
+++ src/MRC.Core/MrcConstants.cs
@@ -5,6 +5,6 @@ public static class MrcConstants
     public const string ProductName = "Main Runner Control";
     public const string CliName = "MRC";
     public const string ReleaseChannel = "stable";
-    public const string TargetMachineName = "Main-PC";
+    public const string TargetMachineName = "DOONCHYSCOMPUTI";
     public const string RunnerRoot = @"D:\Git_Runners_Main";
 }
