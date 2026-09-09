782256ef5e0cf44ea362143341376b06eb1b9a0b
doonchy16-cloud <doonchy16@gmail.com>
2026-09-09 03:36:01 -0800

    test: expose runtime internals to pre-cert diagnostics

--- src/MRC.Core/Properties/AssemblyInfo.cs
+++ src/MRC.Core/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("MRC.Pass2.Tests")]
 [assembly: InternalsVisibleTo("MRC.Pass4.Tests")]
+[assembly: InternalsVisibleTo("MRC.PreCert.Tests")]
