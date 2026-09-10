6f05b76a46b98f435e1372267f457d1b4df7b47d
doonchy16-cloud <doonchy16@gmail.com>
2026-09-09 16:01:19 -0800

    fix: import WPF scrollbar primitives

--- src/MRC.Gui/MainWindow.xaml.cs
+++ src/MRC.Gui/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 using System.Windows;
 using System.Windows.Automation;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
