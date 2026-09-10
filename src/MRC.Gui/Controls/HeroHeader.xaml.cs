15ccc766f58d2b069b0672ccda93f28ade5165c9
doonchy16-cloud <doonchy16@gmail.com>
2026-09-10 09:45:50 -0800

    feat: add HeroHeader display contract

---
+++ src/MRC.Gui/Controls/HeroHeader.xaml.cs
@@ -0,0 +1,53 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MRC.Gui.Controls;
+
+public partial class HeroHeader : UserControl
+{
+    public static readonly DependencyProperty InventoryTextProperty = DependencyProperty.Register(
+        nameof(InventoryText), typeof(string), typeof(HeroHeader), new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty TruthTextProperty = DependencyProperty.Register(
+        nameof(TruthText), typeof(string), typeof(HeroHeader), new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty VersionTextProperty = DependencyProperty.Register(
+        nameof(VersionText), typeof(string), typeof(HeroHeader), new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty AuthorizedProperty = DependencyProperty.Register(
+        nameof(Authorized), typeof(bool), typeof(HeroHeader), new PropertyMetadata(false));
+
+    public HeroHeader()
+    {
+        InitializeComponent();
+    }
+
+    public string InventoryText
+    {
+        get => (string)GetValue(InventoryTextProperty);
+        set => SetValue(InventoryTextProperty, value);
+    }
+
+    public string TruthText
+    {
+        get => (string)GetValue(TruthTextProperty);
+        set => SetValue(TruthTextProperty, value);
+    }
+
+    public string VersionText
+    {
+        get => (string)GetValue(VersionTextProperty);
+        set => SetValue(VersionTextProperty, value);
+    }
+
+    public bool Authorized
+    {
+        get => (bool)GetValue(AuthorizedProperty);
+        set => SetValue(AuthorizedProperty, value);
+    }
+
+    public event RoutedEventHandler? MenuRequested;
+
+    private void Menu_OnClick(object sender, RoutedEventArgs e) =>
+        MenuRequested?.Invoke(this, e);
+}
