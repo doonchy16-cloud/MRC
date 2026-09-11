5a4c92f2b13922ebea884447d346fc2690ff95a8
doonchy16-cloud <doonchy16@gmail.com>
2026-09-10 21:00:29 -0800

    fix: initialize ambient background visual tree

---
+++ src/MRC.Gui/Controls/AmbientBackground.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace MRC.Gui.Controls;
+
+public partial class AmbientBackground : UserControl
+{
+    public AmbientBackground()
+    {
+        InitializeComponent();
+    }
+}
